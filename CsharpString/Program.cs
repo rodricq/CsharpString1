e87936a0f4f8164aa3959defb568402c4238eb61
beverley-dick <beverleydick11@gmail.com>
2023-05-09 17:56:39 +0300

    Add project files.

---
+++ CsharpString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsharpString
+{
+    class Test
+    {
+       public static void Main(string[] args)
+        {
+
+            //create string 
+            string str = "C# Programming";
+            Console.WriteLine("String: " + str);
+
+            //get length of str
+            int length = str.Length;
+            
+            Console.WriteLine("Length: " + length);
+
+            Console.ReadLine();
+        }
+    }
+}
